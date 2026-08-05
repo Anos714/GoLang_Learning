441494438e9d1331fab62b941063132f4518a8aa
Rahul Sain <sainrahul374@gmail.com>
2026-08-05 13:59:23 +0530

    learn about import and packages

---
+++ 03_packages_imports/main.go
@@ -0,0 +1,13 @@
+package main
+
+// import brings external pkgs into the file that u r working or where u actually need it
+import (
+	"fmt"
+	"math"
+)
+
+func main(){
+	// pkgName.FunctionName -> call a function from a pkg
+
+	fmt.Println("square root of 25: ",math.Sqrt(25));
+}
