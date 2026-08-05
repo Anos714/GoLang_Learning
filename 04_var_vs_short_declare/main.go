859d9b5c72146f8d57039a93863ca04eb608812f
Rahul Sain <sainrahul374@gmail.com>
2026-08-05 14:11:01 +0530

    learn short declaration of variables

---
+++ 04_var_vs_short_declare/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main(){
+	  name,age,city,dob,phone,weight := "Rahul Sain",21,"Alwar","06-01-2005","9876543210",67.800;
+	fmt.Printf("Name %s\n Age %d\n City %s\n DOB %s\n Phone %s\n Weight %f\n",name,age,city,dob,phone,weight);
+
+		fmt.Printf(" %+v\n %+v\n %+v\n %+v\n %+v\n %+v",name,age,city,dob,phone,weight);
+
+
+		likes:=2000
+		likes+=200
+
+		fmt.Println("\nlikes: ",likes)
+}
