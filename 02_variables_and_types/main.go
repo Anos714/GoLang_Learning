161d6b080f10df917c1268dcafc441cabebfa735
Rahul Sain <sainrahul374@gmail.com>
2026-08-05 13:54:33 +0530

    learn about variables and their declarations and initializations

---
+++ 02_variables_and_types/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main(){
+	// var name type
+	var name string;
+	name="Rahul Sain";
+
+
+	var year int=2026;
+
+	fmt.Println("name: ",name);
+	fmt.Println("year: ",year);
+
+	var rating float64=4.8;
+	fmt.Println("rating: ",rating)
+}
