ea50a6d59a94f9b87d6aa761b02a2b9bdefbaf37
Rahul Sain <sainrahul374@gmail.com>
2026-08-07 18:41:21 +0530

    learn about error handling and error return pattern in go

---
+++ 19_error_return_pattern/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+)
+
+func main() {
+	// go don't use exceptions for normal failures
+	// functions -> return errors as normal return values
+
+
+	// syntax
+	// val,err := something()
+	// if err != nil {
+	// 	handle error
+	// 	return
+	// }
+
+if err:=run();err!=nil{
+	log.Fatal(err)
+}
+
+	if val, err := checkAge(15); err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Println("Age: ", val)
+	}
+}
+
+
+
+
+func run()error{
+	input:="2"
+	level,err:=parseLevel(input)
+
+if err!=nil{
+	return err
+}
+fmt.Println("Selected level: ",level)
+return nil
+}
+
+func parseLevel(str string)(int,error){
+	if str == "" {
+		return 0, fmt.Errorf("empty string")
+	}
+	val,err:=strconv.Atoi(str)
+	if err!=nil{
+		return 0,fmt.Errorf("Level must be a number")
+	}
+
+	if val<1||val>5{
+		return 0, fmt.Errorf("Level must be between 1 and 5")
+	}
+
+	return val,nil
+}
+
+func checkAge(age int)(int,error){
+	if age<18{
+		return 0,fmt.Errorf("Age must be at least 18")
+	}
+	return age,nil
+}
