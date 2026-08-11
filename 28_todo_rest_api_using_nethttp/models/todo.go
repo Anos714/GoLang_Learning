20ee462390caf8b6ced743bfe7756606b0d33aaa
Rahul Sain <sainrahul374@gmail.com>
2026-08-11 20:28:17 +0530

    refactor: the todo db model

--- 28_todo_rest_api_using_nethttp/models/todo.go
+++ 28_todo_rest_api_using_nethttp/models/todo.go
@@ -6,6 +6,6 @@ type Todo struct{
 	ID uint `gorm:"primaryKey" json:"id"`
 	Title string `gorm:"type:varchar(255);not null" json:"title"`
 	Done bool `gorm:"default:false" json:"done"`
-	CreatedAt time.Time `json"created_at"`
-	UpdatedAt time.Time `json"updated_at"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
